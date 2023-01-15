425c073165de2266436f2e1fd8689d8210ee5481
Rafael da Silva Santos <05202198155@faqi.edu.br>
2023-01-15 05:38:54 -0300

    Simulando o lançamento de exceções com Mockito.
    Implementado no código o envio de e-mails para 'vencedor do leilao com maior lance' e criados testes para caminho feliz e caminho triste (exceção).
    
    Alterei o try/catch do instrutor no chamado do metódo dentro da classe de testes, para um assertThrows do Junit, pois assim o método ainda seria chamado e pareceu mais 'elegante' a solução apresentada com try/catch.

--- java-testes-mock-projeto/src/test/java/br/com/alura/leilao/service/FinalizarLeilaoServiceTest.java
+++ java-testes-mock-projeto/src/test/java/br/com/alura/leilao/service/FinalizarLeilaoServiceTest.java
@@ -4,12 +4,9 @@ import br.com.alura.leilao.dao.LeilaoDao;
 import br.com.alura.leilao.model.Lance;
 import br.com.alura.leilao.model.Leilao;
 import br.com.alura.leilao.model.Usuario;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
 import java.math.BigDecimal;
@@ -17,20 +14,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 class FinalizarLeilaoServiceTest {
     @Mock
     private LeilaoDao leilaoDaoMock;
 
+    @Mock
+    private EnviadorDeEmails enviadorDeEmailsMock;
 //    @InjectMocks
     private FinalizarLeilaoService finalizarLeilaoService;
 
     @BeforeEach
     void setUp() {
         MockitoAnnotations.initMocks(this);
-        this.finalizarLeilaoService = new FinalizarLeilaoService(leilaoDaoMock);
+        this.finalizarLeilaoService = new FinalizarLeilaoService(leilaoDaoMock, enviadorDeEmailsMock);
     }
 
     @Test
@@ -40,10 +38,35 @@ class FinalizarLeilaoServiceTest {
         finalizarLeilaoService.finalizarLeiloesExpirados();
 
         Leilao leilao = leiloes.get(0);
+        Lance lanceVencedor = leilao.getLanceVencedor();
+
         assertTrue(leilao.isFechado());
-        assertEquals(new BigDecimal("900"), leilao.getLanceVencedor().getValor());
+        assertEquals(new BigDecimal("900"), lanceVencedor.getValor());
 
         verify(leilaoDaoMock).salvar(leilao);
+    }
+
+    @Test
+    void deveriaEnviarEmailParaVencedorDoLeilao() {
+        List<Leilao> leiloes = leiloesFake();
+        when(leilaoDaoMock.buscarLeiloesExpirados()).thenReturn(leiloes);
+        finalizarLeilaoService.finalizarLeiloesExpirados();
+
+        Leilao leilao = leiloes.get(0);
+        Lance lanceVencedor = leilao.getLanceVencedor();
+
+        verify(enviadorDeEmailsMock).enviarEmailVencedorLeilao(lanceVencedor);
+    }
+
+    @Test
+    void naoDeveriaEnviarEmailParaVencedorDoLeilaoEmCasoDeErroAoEncerrarLeilao() {
+        List<Leilao> leiloes = leiloesFake();
+        when(leilaoDaoMock.buscarLeiloesExpirados()).thenReturn(leiloes);
+        when(leilaoDaoMock.salvar(any())).thenThrow(RuntimeException.class);
+
+        assertThrows(RuntimeException.class, () -> finalizarLeilaoService.finalizarLeiloesExpirados());
+
+        verifyNoInteractions(enviadorDeEmailsMock);
     }
 
     private List<Leilao> leiloesFake() {
